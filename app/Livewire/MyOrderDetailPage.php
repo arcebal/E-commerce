56ac7222c4f272db390e266edd52f629d89f83b4
John Clifford <ceballosjohnclifford@gmail.com>
2025-11-30 04:31:35 +0800

    Chapter 25 E-commerce

--- app/Livewire/MyOrderDetailPage.php
+++ app/Livewire/MyOrderDetailPage.php
@@ -2,12 +2,34 @@
 
 namespace App\Livewire;
 
+use Livewire\Attributes\Title;
 use Livewire\Component;
+use App\Models\OrderItem;
+use App\Models\Address;
+use App\Models\Order;
 
+
+#[Title('Order Detail')]
 class MyOrderDetailPage extends Component
 {
+
+    public $order_id;
+
+    public function mount($order_id)
+    {
+        $this->order_id = $order_id;
+    }
+
     public function render()
     {
-        return view('livewire.my-order-detail-page');
+        $order = Order::findOrFail($this->order_id);
+        $order_items = OrderItem::with('product')->where('order_id', $this->order_id)->get();
+        $address = Address::where('order_id', $this->order_id)->first();
+        
+        return view('livewire.my-order-detail-page', [
+            'order_items' => $order_items,
+            'address' => $address,
+            'order' => $order
+        ]);
     }
 }
